9386e2e39531ff2366a5fe7ae05162585cc03f50
wingdr <krq2000@126.com>
2024-03-08 14:53:48 +0800

    关闭触发器运行log

--- src/events/eventTrigger.ts
+++ src/events/eventTrigger.ts
@@ -49,7 +49,7 @@ export class EventTrigger {
 
   private wsMainTrigger(event: CustomEvent<any>) {
     if (this.onProcessing) return;
-    if (isDev) this.logger.info("触发器运行中");
+    // if (isDev) this.logger.info("触发器运行中");
     if ( Object.keys(this.eventQueue).indexOf(event.detail.cmd) != -1 ) {
       if (isDev) this.logger.info("事件触发，event =>", {type: "ws-main", cmd: event.detail.cmd});
       this.execEvent(event.detail.cmd, event);
